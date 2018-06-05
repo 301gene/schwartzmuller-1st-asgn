1a14288f1b8c991ca98f6411187dbacd107009d3
Eugene Slesarev <301gene@gmail.com>
2018-06-04 22:58:17 -0400

    Up to step 7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    username: "Scrooge"
+  }
+
+  userNameHandler = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <UserInput changed={this.userNameHandler} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput />
       </div>
     );
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const useroutput = (props) => {
+    return (
+        <div className="UserOutput">
+            <p>A {props.userName} is a person who is stingy with money: {props.userName}s would rather do anything than part with a buck. The novels of Charles Dickens have contributed more than a dozen words that found their way into everyday language. {props.userName}, the chief character from A Christmas Carol, is perhaps the best-known of them all.</p>
+            <p>It has been suggested that Dickens chose the name {props.userName} ("stone (of) help") to reflect the help given to {props.userName} to change his life. The surname may be from the now obscure English verb scrouge, meaning "squeeze" or "press".</p>
+        </div>
+    )
+};
+
+export default useroutput;
